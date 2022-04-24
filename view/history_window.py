de0bc39fa724ee94d28eef35a5471197a63114d1
Buner <rbns71@gamil.com>
2022-04-24 18:32:10 +0200

    Fixed scrollbar in history_window

--- view/history_window.py
+++ view/history_window.py
@@ -1,4 +1,5 @@
-from tkinter import ANCHOR, END, LEFT, RIGHT, VERTICAL, Y, Button, Entry, Listbox, Scrollbar, StringVar, Toplevel, messagebox
+from pickle import FRAME
+from tkinter import ANCHOR, END, EXTENDED, LEFT, RIGHT, VERTICAL, Y, Button, Entry, Listbox, Scrollbar, StringVar, Toplevel, Frame
 
 # When "counter" is True, allow to open the history
 global counter
@@ -18,11 +19,13 @@ def open():
         topHistory.resizable(0, 0)
         counter = False
 
-        scroll = Scrollbar(topHistory, orient=VERTICAL)
+        history_frame = Frame(topHistory)
 
-        # Content when the items place
+        scroll = Scrollbar(history_frame, orient=VERTICAL)
+
+        # Content where put the items 
         history_box = Listbox(
-            topHistory,
+            history_frame,
             yscrollcommand=scroll.set,
             borderwidth=0,
             bg="black",
@@ -30,30 +33,26 @@ def open():
             highlightthickness=0,
             selectbackground="#3290F1",
             font=10,
-
+            selectmode=EXTENDED,
             )
-        history_box.pack(fill="both", expand=1)   
+ 
 
         scroll.config(command=history_box.yview)
         scroll.pack(side=RIGHT, fill=Y)
 
+        history_frame.pack(fill="both", expand=1)
+        history_box.pack(fill="both", expand=1)  
+
+
+
+
+
         manual_item = StringVar()
-        manual_entry = Entry(topHistory, textvariable=manual_item)
-        manual_entry.pack(side=LEFT)
-
-
-
-
-
-
-
-
-
-
-
+        manual_entry = Entry(history_frame, textvariable=manual_item, width=29)
+        manual_entry.pack(side=LEFT, fill=Y)
 
         btn_close = Button(
-            topHistory,
+            history_frame,
             text="Cerrar",
             cursor="fleur",
             bg="black",
@@ -62,11 +61,11 @@ def open():
             activebackground="#BDBDBD",
             command=lambda:[topHistory.destroy(), counter_on()] 
         )
-        btn_close.place(x=210, y=10)
+        btn_close.place(x=210, y=800)
 
 
         btn_clean = Button(
-            topHistory,
+            history_frame,
             text="Limpiar",
             cursor="fleur",
             bg="black",
@@ -75,23 +74,10 @@ def open():
             activebackground="#BDBDBD",
             command=lambda:history_box.delete(0, END) 
         )
-        btn_clean.place(x=210, y=50)
-
-
-        btn_insert = Button(
-            topHistory,
-            text="Testear",
-            cursor="fleur",
-            bg="black",
-            fg="white",
-            activeforeground="black",
-            activebackground="#BDBDBD",
-            command=lambda:history_box.insert(0, "  testing...") 
-        )
-        btn_insert.place(x=210, y=100)
+        btn_clean.place(x=200, y=850)
 
         btn_delete = Button(
-            topHistory,
+            history_frame,
             text="Eliminar",
             cursor="fleur",
             bg="black",
@@ -100,10 +86,10 @@ def open():
             activebackground="#BDBDBD",
             command=lambda:history_box.delete(ANCHOR)
         )
-        btn_delete.place(x=210, y=150)
+        btn_delete.place(x=200, y=900)
 
         btn_add = Button(
-            topHistory,
+            history_frame,
             text="Add",
             cursor="fleur",
             bg="black",
@@ -112,7 +98,7 @@ def open():
             activebackground="#BDBDBD",
             command=lambda: history_box.insert(0, manual_item.get())
         )
-        btn_add.place(x=210, y=500)
+        btn_add.pack(side=RIGHT)
 
     
 
